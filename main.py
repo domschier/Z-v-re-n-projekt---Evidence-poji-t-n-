ebc869cc5ef450602cad98a1e0b4e2d4ad013b2b
domschier <160162565+domschier@users.noreply.github.com>
2024-02-15 22:32:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from evidence import Evidence
+from databaze import Databaze
+
+
+databaze = Databaze()
+if __name__ == '__main__':
+    databaze.main()
+
+evidence = Evidence()
+evidence.main(databaze)
